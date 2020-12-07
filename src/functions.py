b0c61cd6a46570f6459034e8753275132e2d79d8
Håvard <8015540+HGodal@users.noreply.github.com>
2020-12-07 22:38:25 +0100

    Renaming files

--- src/functions.py
+++ src/functions.py
@@ -25,7 +25,6 @@ Functions by Author:
         simulate_agent
         mutation_rate
         save_frames_as_gif
-        partial_fit
 
     Marius Sørensen:
         create_new_network
@@ -38,7 +37,6 @@ Functions by Author:
     Håvard Godal:
         simulate_agent
         de_crossover
-        breedCrossover
         de_crossover_classic
 
     Johanna Kinstad:
@@ -47,8 +45,6 @@ Functions by Author:
     Vegard Rongve:
         mutationFunc_W_B
         nnPerformance
-        crossoverSinglePoint
-        mutationFunc
 """
 
 from sklearn.neural_network import MLPClassifier
@@ -338,111 +334,6 @@ def save_frames_as_gif(frames, path='./', filename='gym_animation.gif'):
     anim.save(path + filename, writer='pillow', fps=60)
 
 
-########################
-# Deprecated functions #
-########################
-
-
-def partial_fit(best_trained, best_network, env):
-    """Partial fit neural netowork to actions of best agent.
-
-    Author: Bjørn Christian Weinbach
-
-    Args:
-        best_trained (MLPClassifier): [Neural network to be trained]
-        best_network (MLPClassifier): [Neural network that scored high]
-        env (OpenAI gym): [Environment]
-
-    Returns:
-        [best_trained]: [partially fitted neural netork]
-    """
-    observation = env.reset()
-    score = 0
-    actions = np.empty(5)
-    trainingx = []
-    trainingy = []
-    actions
-    terminate = False
-
-    while not(terminate):
-        j = 0
-        action = int(best_network.predict(
-            observation.reshape(1, -1).reshape(1, -1)))
-        if j > 5 and sum(actions) % 5 == 0:
-            action = env.action_space.sample()
-        observation, reward, done, _ = env.step(action)
-        trainingx.append(observation)
-        trainingy.append(action)
-        score += reward
-        j += 1
-        actions[j % 5] = action
-        terminate = done
-
-    best_trained.partial_fit(
-        trainingx,
-        trainingy
-    )
-
-    return best_trained
-
-
-def breedCrossover(nn1, nn2):
-    """Breeds a child from 2 parents using crossover.
-
-    Author: Håvard Godal
-
-    Args:
-        nn1 ([MLPClassifier]): [Neural network parent nr 1]
-        nn2 ([MLPClassifier]): [Neural network parent nr 2]
-
-    Returns:
-        [children]: [List of two children containing coefs_ and intercepts_]
-    """
-    # Choosing either input -> hidden-layer or hidden-layer -> output
-    layer = random.randint(0, 1)
-
-    child1 = []
-    child2 = []
-
-    for i in range(2):
-        if i == 0:
-            param1 = nn1.coefs_
-            param2 = nn2.coefs_
-        else:
-            param1 = nn1.intercepts_
-            param2 = nn2.intercepts_
-
-        shape = param2[layer].shape
-
-        paramFlat1 = np.ravel(param1[layer])
-        paramFlat2 = np.ravel(param2[layer])
-
-        indexes = sorted([int(random.random() * len(paramFlat1)),
-                          int(random.random() * len(paramFlat1))])
-
-        newFlatParam = copy.copy(paramFlat2)
-        newFlatParam[indexes[0]:indexes[1]] = \
-            paramFlat1[indexes[0]:indexes[1]]
-
-        newParam = []
-        newParam.insert(layer, np.array(newFlatParam).reshape(shape))
-        newParam.insert(1 - layer, param2[1 - layer])
-
-        child1.append(newParam)
-
-        newFlatParam = copy.copy(paramFlat1)
-        newFlatParam[indexes[0]:indexes[1]] = \
-            paramFlat2[indexes[0]:indexes[1]]
-
-        newParam = []
-        newParam.insert(layer, np.array(newFlatParam).reshape(shape))
-        newParam.insert(1 - layer, param1[1 - layer])
-
-        child2.append(newParam)
-
-    return [child1, child2]
-
-
 def de_crossover_classic(agent, randomAgents):
     """Create new weights and biases through a de-crossover.
 
@@ -499,90 +390,3 @@ def de_crossover_classic(agent, randomAgents):
         newintercepts.append(agentInterceptsFlat.reshape(shape))
 
     return newcoefs, newintercepts
-
-
-def crossoverSinglePoint(parent1, parent2):
-    """Breeds two children from 2 parents using single point crossover.
-
-    Author: Vegard Rongve
-
-    Args:
-        parent1 ([MLPClassifier]): [Neural network parent nr 1]
-        parent2 ([MLPClassifier]): [Neural network parent nr 2]
-
-    Returns:
-        [children]: [List of two children containing coefs_ and intercepts_]
-    """
-    child1 = []
-    child2 = []
-
-    for item in range(2):
-        if item == 0:
-            p1 = parent1.coefs_
-            p2 = parent2.coefs_
-        else:
-            p1 = parent1.intercepts_
-            p2 = parent2.intercepts_
-
-        for i in range(2):
-            if i == 0:
-                param1 = p1[0]
-                param2 = p2[0]
-            else:
-                param1 = p1[1]
-                param2 = p2[1]
-
-            shape = param1.shape
-            flatParam1 = np.ravel(param1)
-            flatParam2 = np.ravel(param2)
-
-            randIndex = int(random.random()*flatParam1.size)
-
-            ch1 = np.concatenate(
-                (flatParam1[:randIndex], flatParam2[randIndex:]))
-            ch2 = np.concatenate(
-                (flatParam2[:randIndex], flatParam1[randIndex:]))
-
-            # Reshape
-            ch1Reshape = np.reshape(ch1, shape)
-            ch2Reshape = np.reshape(ch2, shape)
-
-            child1.append(ch1Reshape)
-            child2.append(ch2Reshape)
-
-    return [child1[:2], child1[2:]], [child2[:2], child2[2:]]
-
-
-def mutationFunc(agent, mutation_rate):
-    """Mutate agents weights and biases.
-
-    Author: Vegard Rongve
-
-    Args:
-        agent ([MLPClassifier]): [Neural Network of agent]
-        mutation_rate ([float]): [Probability of mutation]
-
-    Returns:
-        [agent]: [Mutated agent]
-    """
-    for item in range(2):
-        if item == 0:
-            node_item = agent.coefs_
-        else:
-            node_item = agent.intercepts_
-
-        for i in range(len(node_item)):
-            for swappedRow in range(len(node_item[i])):
-                if (random.random() < mutation_rate):
-                    rowToSwapWith = int(random.random()*len(node_item[i]))
-                    row1 = copy.copy(node_item[i][swappedRow])
-                    row2 = copy.copy(node_item[i][rowToSwapWith])
-                    node_item[i][swappedRow] = row2
-                    node_item[i][rowToSwapWith] = row1
-
-        if item == 0:
-            agent.coefs_ = node_item
-        else:
-            agent.intercepts_ = node_item
-
-    return agent
